41a435527d6bcd17daced174031be2b1f83f6502
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-12-18 23:39:56 +0000

    V1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import keyboard as keyboard
 
 from data import SharkData
 
-counter = 0
+counter, shark = 0, 1
 active = True
 
 
@@ -14,34 +14,32 @@ def stop():
     active = False
 
 
-def keypress(keys):
-    keyboard.send(keys, do_release=False)
-    time.sleep(random.uniform(0.1, 0.3))
-    keyboard.release(keys)
+def keypress(k):
+    keyboard.send(k, do_release=False)
+    wait = random.uniform(0.06, 0.3)
+    print(f"{keys}, {wait} ")
+
+    time.sleep(wait)
+    keyboard.release(k)
 
 
 sharks = SharkData()
-
-shark_1 = sharks.sort_data(1)
-# sharks.describe_data(shark_1)
+shark_1 = sharks.sort_data(shark)
 
 length = shark_1["Shark"].count()
-
-print("Keyboard read and waiting!")
-
 keyboard.add_hotkey('shift+2', lambda: stop())
+
+print(f"Keyboard ready and waiting! Input Length {length} for shark {shark}")
 keyboard.wait('shift+1')
 
 while active:
     s = shark_1.iloc[counter]
-    up, right = s['up'], s['right']
 
     keys = [
-        "s" if up else "down",
-        "right" if right else "left"
+        "s" if s['up'] else "down",
+        "right" if s['right'] else "left"
     ]
 
-    print(f"Keys pressed: {keys}")
     keypress(keys)
 
     counter = (counter + 1) if counter < length - 1 else 0
